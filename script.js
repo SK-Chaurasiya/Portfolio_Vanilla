83dbaf85b9c6ded9ceffa997936062774e81182a
White-Hat-Designer <97239651+SK-Chaurasiya@users.noreply.github.com>
2023-11-25 21:00:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
+// alert('hello')
 
-
-// javascript for navigation bar effects on scroll
+// Javascript for navigation bar effects on scroll
 window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle('sticky', window.scrollY > 0);
 });
 
 
-// javascript for responsive navigation sidebar menu
+// Javascript for responsive navigation sidebar menu
 const menuBtn = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".navigation");
 const navigationItems = document.querySelectorAll(".navigation a")
 
-menuBtn.addEventListener("click", () => {
+menuBtn.addEventListener("click",  () => {
   menuBtn.classList.toggle("active");
   navigation.classList.toggle("active");
 });
@@ -25,33 +25,33 @@ navigationItems.forEach((navigationItem) => {
 });
 
 
-// javascript for scroll to top button
+// Javascript for scroll to top button
 const scrollBtn = document.querySelector(".scrollToTop-btn");
 
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", function(){
   scrollBtn.classList.toggle("active", window.scrollY > 500);
 });
 
-
-// javascript for scroll back to top on click scroll-to-top button
+// Javascript for scroll back to top on click scroll-to-top button
 scrollBtn.addEventListener("click", () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
 
 
-// javascript for reveal website elements on scroll
+
+// Javascript for reveal website elements on scroll
 window.addEventListener("scroll", reveal);
 
-function reveal() {
+function reveal(){
   var reveals = document.querySelectorAll(".reveal");
 
-  for (var i = 0; i < reveals.length; i++) {
+  for(var i = 0; i < reveals.length; i++){
     var windowHeight = window.innerHeight;
     var revealTop = reveals[i].getBoundingClientRect().top;
     var revealPoint = 50;
 
-    if (revealTop < windowHeight - revealPoint) {
+    if(revealTop < windowHeight - revealPoint){
       reveals[i].classList.add("active");
     }
   }
